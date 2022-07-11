8c0abcaf92ed73ad4d1b370c48cd205d0b4f8545
SNEHAKMALOJI123 <snehamaloji@gmail.com>
2022-07-11 20:25:14 +0530

    Create pg.php

---
+++ Project-6/pg.php
@@ -0,0 +1,28 @@
+
+<?php
+$user = "SNEHA MALOJI";
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title> PROJECT-6 </title>
+</head>
+
+<body>
+    <h2><?php echo $user; ?>,Welcome to Sample PHP Script </h2>
+    <?php
+    print "<h3> REFRESH PAGE </h3>";
+    $name = "counter.txt";
+    $file = fopen($name, "r");
+    $hits = fscanf($file, "%d");
+    fclose($file);
+    $hits[0]++;
+    $file = fopen($name, "w");
+    fprintf($file, "%d", $hits[0]);
+    fclose($file);
+    print "Total number of views: " . $hits[0];
+    ?>
+</body>
+
+</html>
